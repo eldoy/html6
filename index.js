6bc2d82a9eb3ac2c9239abb87a408dffcdbfb8c9
Vidar Eldøy <vidar@eldoy.com>
2025-07-15 02:52:42 +0200

    fix: speed up templates

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var compile = require('./lib/compile.js')
 
 module.exports = function (opt = {}) {
-  opt.templates ||= {}
-
   return {
     compile: function (source) {
       return compile(source, opt)
