aa764d63f30e41bb295dd73f3b6de52df59c1ea5
Vidar Eldøy <vidar@eldoy.com>
2025-07-15 05:18:49 +0200

    fix: cave in

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var compile = require('./lib/compile.js')
 
 module.exports = function (opt = {}) {
+  opt.templates ||= {}
   return {
     compile: function (source) {
       return compile(source, opt)
