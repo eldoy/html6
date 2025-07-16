d2d644e0029bdd0ee6507c7537775d7d5cb78a1d
Vidar Eldøy <vidar@eldoy.com>
2025-07-16 05:09:05 +0200

    fix: toposort templates

--- docs/TODO.md
+++ docs/TODO.md
@@ -3,3 +3,8 @@
 - [ ] Pipes
 - [ ] Defaults for slots
 - [ ] Named slots
+- [ ] Data on templates
+- [ ] ${reply} syntax on attributes?
+  - will make it possible to allow strings etc. directly on the prop
+
+  <comment comment="${reply}"></comment>
--- spec/tests/html.test.js
+++ spec/tests/html.test.js
@@ -39,3 +39,48 @@ test('slot template', async ({ t }) => {
 
   t.equal(result, '<div>hello</div>')
 })
+
+test('simple props', async ({ t }) => {
+  var source = '<div>${hello}</div>'
+  var renderer = html.compile(source)
+
+  var result = renderer.render({ hello: 'world' })
+
+  t.equal(result, '<div>world</div>')
+})
+
+test('deep props', async ({ t }) => {
+  var source = '<div>${project.title}</div>'
+  var renderer = html.compile(source)
+
+  var result = renderer.render({ project: { title: 'world' } })
+
+  t.equal(result, '<div>world</div>')
+})
+
+test('template props', async ({ t }) => {
+  var source = '<card greeting="greeting"></card>'
+  var templates = ['<template id="card"><div>${greeting}</div></template>']
+  var opt = { templates }
+
+  var renderer = html.compile(source, opt)
+
+  var result = renderer.render({ greeting: 'hello' })
+
+  t.equal(result, '<div>hello</div>')
+})
+
+test('deep template props', async ({ t }) => {
+  var source = '<card greeting="greeting"></card>'
+  var templates = [
+    '<template id="card"><heading greeting="greeting"></heading></template>',
+    '<template id="heading"><div>${greeting}</div></template>'
+  ]
+  var opt = { templates }
+
+  var renderer = html.compile(source, opt)
+
+  var result = renderer.render({ greeting: 'hello' })
+
+  t.equal(result, '<div>hello</div>')
+})
