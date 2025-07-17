229329ec4fb299df1b44b6e9efb123e40c0d52de
Vidar Eldøy <vidar@eldoy.com>
2025-07-17 07:21:38 +0200

    fix: prepare pipes

--- spec/tests/literal.test.js
+++ spec/tests/literal.test.js
@@ -2,52 +2,52 @@ var literal = require('../../lib/literal.js')
 
 test('empty', async function ({ t }) {
   var result = literal('')
-  t.equal(result, 'esc()')
+  t.equal(result, '_.esc()')
 })
 
 test('value', async function ({ t }) {
   var result = literal('hello')
-  t.equal(result, 'esc(hello)')
+  t.equal(result, '_.esc(hello)')
 })
 
 test('single pipe', async function ({ t }) {
   var result = literal('hello | pipe')
-  t.equal(result, 'pipe(esc(hello))')
+  t.equal(result, '_.pipe(_.esc(hello))')
 })
 
 test('pipe args - var', async function ({ t }) {
   var result = literal('hello | pipe a')
-  t.equal(result, 'pipe(esc(hello),a)')
+  t.equal(result, '_.pipe(_.esc(hello),a)')
 })
 
 test('pipe args - string', async function ({ t }) {
   var result = literal('hello | pipe "hello"')
-  t.equal(result, 'pipe(esc(hello),"hello")')
+  t.equal(result, '_.pipe(_.esc(hello),"hello")')
 })
 
 test('pipe args - number', async function ({ t }) {
   var result = literal('hello | pipe 5')
-  t.equal(result, 'pipe(esc(hello),5)')
+  t.equal(result, '_.pipe(_.esc(hello),5)')
 })
 
 test('pipe args - array', async function ({ t }) {
   var result = literal('hello | pipe [1,2,3]')
-  t.equal(result, 'pipe(esc(hello),[1,2,3])')
+  t.equal(result, '_.pipe(_.esc(hello),[1,2,3])')
 })
 
 test('pipe args - object', async function ({ t }) {
   var result = literal('hello | pipe {hello: greeting }')
-  t.equal(result, 'pipe(esc(hello),{hello: greeting })')
+  t.equal(result, '_.pipe(_.esc(hello),{hello: greeting })')
 })
 
 test('multi pipe', async function ({ t }) {
   var result = literal('hello | pipe | list')
-  t.equal(result, 'list(pipe(esc(hello)))')
+  t.equal(result, '_.list(_.pipe(_.esc(hello)))')
 })
 
 test('multi pipe - args', async function ({ t }) {
   var result = literal('hello | pipe { hello: "world" } | list 5')
-  t.equal(result, 'list(pipe(esc(hello),{ hello: "world" }),5)')
+  t.equal(result, '_.list(_.pipe(_.esc(hello),{ hello: "world" }),5)')
 })
 
 test('raw', async function ({ t }) {
