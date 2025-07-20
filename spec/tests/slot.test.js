7e16a48258e81e17067759c4650f0dcaba2a0797
Vidar Eldøy <vidar@eldoy.com>
2025-07-20 06:35:17 +0200

    fix: slot fallback

--- spec/tests/slot.test.js
+++ spec/tests/slot.test.js
@@ -1,3 +1,4 @@
+var parser = require('../../lib/parser.js')
 var slot = require('../../lib/slot.js')
 
 test('default empty', async ({ t }) => {
@@ -41,6 +42,41 @@ test('named - attributes', async ({ t }) => {
 
   var result = slot(node)
   var expected = '${slots.content}'
+
+  t.equal(result, expected)
+})
+
+test('default - fallback', async ({ t }) => {
+  var node = {
+    type: 'element',
+    tagName: 'slot',
+    attributes: [],
+    children: [{ type: 'text', content: 'This is the default fallback text.' }]
+  }
+
+  var result = slot(node)
+  var expected = '${slots.default || `This is the default fallback text.`}'
+
+  t.equal(result, expected)
+})
+
+test('named - fallback', async ({ t }) => {
+  var node = {
+    type: 'element',
+    tagName: 'slot',
+    attributes: [{ key: 'name', value: 'footer' }],
+    children: [
+      {
+        type: 'element',
+        tagName: 'i',
+        attributes: [],
+        children: [{ type: 'text', content: 'Default footer text.' }]
+      }
+    ]
+  }
+
+  var result = slot(node)
+  var expected = '${slots.footer || `<i>Default footer text.</i>`}'
 
   t.equal(result, expected)
 })
