90074fa50874986f229c7d6c62f0ed9a7ec5d8f4
Vidar Eldøy <vidar@eldoy.com>
2025-07-21 03:26:22 +0200

    fix: support inline templates

---
+++ spec/tests/inline.js
@@ -0,0 +1,63 @@
+var inline = require('../../lib/inline.js')
+
+test('with templates', async ({ t }) => {
+  var page = [
+    '<div class="main">',
+    '  <h1>Hello World</h1>',
+    '</div>',
+    '',
+    '<p>Some other content here.</p>',
+    '',
+    '<template id="card">',
+    '  <div class="card">A simple card</div>',
+    '</template>',
+    '',
+    '<template id="user-profile">',
+    '  <div class="profile">',
+    '    <span>user</span>',
+    '  </div>',
+    '</template>'
+  ].join('\n')
+
+  var expectedSource = [
+    '<div class="main">',
+    '  <h1>Hello World</h1>',
+    '</div>',
+    '',
+    '<p>Some other content here.</p>'
+  ].join('\n')
+
+  var expectedComponent1 = [
+    '<template id="card">',
+    '  <div class="card">A simple card</div>',
+    '</template>'
+  ].join('\n')
+
+  var expectedComponent2 = [
+    '<template id="user-profile">',
+    '  <div class="profile">',
+    '    <span>user</span>',
+    '  </div>',
+    '</template>'
+  ].join('\n')
+
+  var [source, components] = inline(page)
+
+  t.equal(source, expectedSource)
+  t.equal(components.length, 2)
+  t.equal(components[0], expectedComponent1)
+  t.equal(components[1], expectedComponent2)
+})
+
+test('with templates', async ({ t }) => {
+  var page = [
+    '<div>',
+    '    <p>This is a page with no templates.</p>',
+    '</div>'
+  ].join('\n')
+
+  var [source, components] = inline(page)
+
+  t.equal(source, page)
+  t.equal(components.length, 0)
+})
--- spec/tests/prepare.test.js
+++ spec/tests/prepare.test.js
@@ -25,3 +25,29 @@ test('simple', async ({ t }) => {
   t.equal(result.card.tree[0].children[0].type, 'text')
   t.equal(result.card.tree[0].children[0].content, 'hello')
 })
+
+test('inline', async ({ t }) => {
+  var components = [
+    '<template id="cards"><card></card></template>',
+    '<template id="card"><div>hello</div></template>',
+    '<template id="card"><div>inline</div></template>'
+  ]
+
+  var result = prepare(components)
+
+  t.equal(result.cards.name, 'cards')
+  t.equal(result.cards.html, '<card></card>')
+  t.equal(result.cards.tree.length, 1)
+  t.equal(result.cards.tree[0].type, 'element')
+  t.equal(result.cards.tree[0].tagName, 'card')
+
+  t.equal(result.card.name, 'card')
+  t.equal(result.card.html, '<div>inline</div>')
+  t.equal(result.card.tree.length, 1)
+
+  t.equal(result.card.tree[0].type, 'element')
+  t.equal(result.card.tree[0].tagName, 'div')
+  t.equal(result.card.tree[0].children.length, 1)
+  t.equal(result.card.tree[0].children[0].type, 'text')
+  t.equal(result.card.tree[0].children[0].content, 'inline')
+})
