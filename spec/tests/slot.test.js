f0e777616cf7a0c6cd5d269b508db742aec6e27f
Vidar Eldøy <vidar@eldoy.com>
2025-07-21 04:12:42 +0200

    fix: remove named slots

--- spec/tests/slot.test.js
+++ spec/tests/slot.test.js
@@ -15,37 +15,6 @@ test('default empty', async ({ t }) => {
   t.equal(result, expected)
 })
 
-test('named', async ({ t }) => {
-  var node = {
-    type: 'element',
-    tagName: 'slot',
-    attributes: [{ key: 'name', value: 'header' }],
-    children: []
-  }
-
-  var result = slot(node)
-  var expected = '${slots.header}'
-
-  t.equal(result, expected)
-})
-
-test('named - attributes', async ({ t }) => {
-  var node = {
-    type: 'element',
-    tagName: 'slot',
-    attributes: [
-      { key: 'class', value: 'title' },
-      { key: 'name', value: 'content' }
-    ],
-    children: []
-  }
-
-  var result = slot(node)
-  var expected = '${slots.content}'
-
-  t.equal(result, expected)
-})
-
 test('default - fallback', async ({ t }) => {
   var node = {
     type: 'element',
@@ -56,27 +25,6 @@ test('default - fallback', async ({ t }) => {
 
   var result = slot(node)
   var expected = '${slots.default || `This is the default fallback text.`}'
-
-  t.equal(result, expected)
-})
-
-test('named - fallback', async ({ t }) => {
-  var node = {
-    type: 'element',
-    tagName: 'slot',
-    attributes: [{ key: 'name', value: 'footer' }],
-    children: [
-      {
-        type: 'element',
-        tagName: 'i',
-        attributes: [],
-        children: [{ type: 'text', content: 'Default footer text.' }]
-      }
-    ]
-  }
-
-  var result = slot(node)
-  var expected = '${slots.footer || `<i>Default footer text.</i>`}'
 
   t.equal(result, expected)
 })
