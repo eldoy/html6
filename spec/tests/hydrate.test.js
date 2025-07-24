a838ab2e8b23ac3e7340d472e0a854a67201698e
Vidar Eldøy <vidar@eldoy.com>
2025-07-25 01:38:39 +0200

    fix: hydrate

--- spec/tests/hydrate.test.js
+++ spec/tests/hydrate.test.js
@@ -1,7 +1,54 @@
 var hydrate = require('../../lib/hydrate.js')
 
 test('empty source', async function ({ t }) {
-  var source = ''
-  var result = hydrate(source)
+  var store = new Map()
+  var result = hydrate('', store, () => '')
   t.equal(result, '')
+})
+
+test('single mask - lit', async function ({ t }) {
+  var store = new Map()
+  store.set('__::MASK_lit_0_::__', '42')
+  var result = hydrate('value: __::MASK_lit_0_::__', store, (val, type) => val)
+  t.equal(result, 'value: 42')
+})
+
+test('multiple masks - mixed types', async function ({ t }) {
+  var store = new Map()
+  store.set('__::MASK_if_0_::__', 'if(x)')
+  store.set('__::MASK_slot_1_::__', '<slot>')
+  store.set('__::MASK_map_2_::__', '[1,2,3].map(f)')
+  store.set('__::MASK_comp_3_::__', '<Component/>')
+  store.set('__::MASK_lit_4_::__', '"text"')
+
+  var input =
+    '__::MASK_if_0_::__; __::MASK_slot_1_::__; __::MASK_map_2_::__; __::MASK_comp_3_::__; __::MASK_lit_4_::__'
+  var result = hydrate(input, store, (val, type) => `[${type}:${val}]`)
+
+  var expected =
+    '[if:if(x)]; [slot:<slot>]; [map:[1,2,3].map(f)]; [comp:<Component/>]; [lit:"text"]'
+  t.equal(result, expected)
+})
+
+test('repeated masks', async function ({ t }) {
+  var store = new Map()
+  store.set('__::MASK_lit_0_::__', 'X')
+  var input = '__::MASK_lit_0_::__ __::MASK_lit_0_::__ __::MASK_lit_0_::__'
+  var result = hydrate(input, store, (val, type) => val + val)
+  t.equal(result, 'XX XX XX')
+})
+
+test('missing mask in store', async function ({ t }) {
+  var store = new Map()
+  var input = 'before __::MASK_lit_0_::__ after'
+  var result = hydrate(input, store, (val, type) => val || 'MISSING')
+  t.equal(result, 'before MISSING after')
+})
+
+test('type is passed correctly', async function ({ t }) {
+  var store = new Map()
+  store.set('__::MASK_if_0_::__', 'condition')
+  var input = '__::MASK_if_0_::__'
+  var result = hydrate(input, store, (val, type) => type)
+  t.equal(result, 'if')
 })
