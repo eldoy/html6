c159e597cbd476caaa66acc768c00865642b3ae4
Vidar Eldøy <vidar@eldoy.com>
2025-07-25 01:57:29 +0200

    fix: tests

--- spec/tests/expression-test.js
+++ spec/tests/expression-test.js
@@ -1,15 +1,13 @@
 var expression = require('../../lib/expression.js')
 
-globalThis.__HTML6_PIPE_SKIP__ = true
-
 test('replace single expression', async function ({ t }) {
   var result = expression('Hello {name}', (expr) => `[${expr}]`)
-  t.equal(result, 'Hello [name]')
+  t.equal(result, 'Hello [_.esc(name)]')
 })
 
 test('replace multiple expressions', async function ({ t }) {
   var result = expression('{a} + {b} = {c}', (expr) => `<${expr}>`)
-  t.equal(result, '<a> + <b> = <c>')
+  t.equal(result, '<_.esc(a)> + <_.esc(b)> = <_.esc(c)>')
 })
 
 test('ignore invalid expression (disallowed function)', async function ({ t }) {
@@ -29,17 +27,17 @@ test('escaped literal is not replaced', async function ({ t }) {
 
 test('nested braces allowed in expression', async function ({ t }) {
   var result = expression('Value: {a + {b: 1}.b}', (expr) => `[${expr}]`)
-  t.equal(result, 'Value: [a + {b: 1}.b]')
+  t.equal(result, 'Value: [_.esc(a + {b: 1}.b)]')
 })
 
 test('expression with string and escaped quote', async function ({ t }) {
   var result = expression('Len: { "a\\"b".length }', (expr) => `(${expr})`)
-  t.equal(result, 'Len: ("a\\"b".length)')
+  t.equal(result, 'Len: (_.esc("a\\"b".length))')
 })
 
 test('expression at start and end', async function ({ t }) {
   var result = expression('{x} is {y}', (expr) => expr.toUpperCase())
-  t.equal(result, 'X is Y')
+  t.equal(result, '_.ESC(X) is _.ESC(Y)')
 })
 
 test('no expression returns original string', async function ({ t }) {
