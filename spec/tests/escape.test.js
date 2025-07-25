14d06df923c9013f1c369b80e146e10fab7a09e4
Vidar Eldøy <vidar@eldoy.com>
2025-07-25 09:03:17 +0200

    fix: escape text

--- spec/tests/escape.test.js
+++ spec/tests/escape.test.js
@@ -2,8 +2,16 @@ var escape = require('../../lib/escape.js')
 var compile = require('../../lib/compile.js')
 
 test('function', async ({ t }) => {
-  var input = 'value with `backtick`, ${dollar}, and \\backslash\\'
+  var input = '`backtick`, ${dollar}, and \\backslash\\'
   var result = escape(input)
-  var expected = 'value with \\`backtick\\`, \\${dollar}, and \\\\backslash\\\\'
+  var expected = '\\`backtick\\`, \\${dollar}, and \\\\backslash\\\\'
+  t.equal(result, expected)
+})
+
+test('text', async ({ t }) => {
+  var page = '<div>`backtick`, ${dollar}, and \\backslash\\</div>'
+  var expected = '<div>`backtick`, ${dollar}, and \\backslash\\</div>'
+  var renderer = compile(page)
+  var result = renderer.render({})
   t.equal(result, expected)
 })
