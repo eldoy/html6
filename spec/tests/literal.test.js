3ec05f074a3185ca359a717c8d7c97bb3d401c38
Vidar Eldøy <vidar@eldoy.com>
2025-07-30 04:57:07 +0200

    wip: double brace

--- spec/tests/literal.test.js
+++ spec/tests/literal.test.js
@@ -6,46 +6,46 @@ test('no braces', async function ({ t }) {
 })
 
 test('simple expression', async function ({ t }) {
-  var result = literal('hello {x}')
+  var result = literal('hello {{x}}')
   t.equal(result, true)
 })
 
 test('empty braces', async function ({ t }) {
-  var result = literal('{}')
+  var result = literal('{{}}')
   t.equal(result, true)
 })
 
 test('brace at end only', async function ({ t }) {
-  var result = literal('ends with {')
+  var result = literal('ends with {{')
   t.equal(result, false)
 })
 
 test('escaped opening brace', async function ({ t }) {
-  var result = literal('\\{x}')
+  var result = literal('\\{{x}}')
   t.equal(result, true)
 })
 
 test('only closing brace', async function ({ t }) {
-  var result = literal('just } here')
+  var result = literal('just }} here')
   t.equal(result, false)
 })
 
 test('multiple expressions', async function ({ t }) {
-  var result = literal('{a} and {b}')
+  var result = literal('{{a}} and {{b}}')
   t.equal(result, true)
 })
 
 test('nested braces', async function ({ t }) {
-  var result = literal('{a + {b}}')
+  var result = literal('{{a + {b}}}')
   t.equal(result, true)
 })
 
 test('expression in middle', async function ({ t }) {
-  var result = literal('before {x} after')
+  var result = literal('before {{x}} after')
   t.equal(result, true)
 })
 
 test('dollar expression', async function ({ t }) {
   var result = literal('${x}')
-  t.equal(result, true)
+  t.equal(result, false)
 })
