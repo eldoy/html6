477e067f36d3bf3b631bbd234d4aba724eb0373a
Agustin <agustinbrage19@gmail.com>
2025-07-30 20:27:41 -0300

    test: add cases for escaping on slots tests

--- spec/tests/slot.test.js
+++ spec/tests/slot.test.js
@@ -38,3 +38,79 @@ test('default - fallback', async ({ t }) => {
   t.equal(content, '__::MASK_slot_0_::__')
   t.equal(store, 'slots.default || `This is the default fallback text.`')
 })
+
+test('default - fallback with backticks', async ({ t }) => {
+  var node = {
+    type: 'element',
+    tagName: 'slot',
+    attributes: [],
+    children: [{ type: 'text', content: '`text.' }]
+  }
+
+  var opt = { store: new Map() }
+
+  slot(node, opt)
+
+  var content = node.content
+  var store = opt.store.get(content)
+
+  t.equal(content, '__::MASK_slot_0_::__')
+  t.equal(store, 'slots.default || ``text.`')
+})
+
+test('default - fallback with dollar', async ({ t }) => {
+  var node = {
+    type: 'element',
+    tagName: 'slot',
+    attributes: [],
+    children: [{ type: 'text', content: '${text}.' }]
+  }
+
+  var opt = { store: new Map() }
+
+  slot(node, opt)
+
+  var content = node.content
+  var store = opt.store.get(content)
+
+  t.equal(content, '__::MASK_slot_0_::__')
+  t.equal(store, 'slots.default || `${text}.`')
+})
+
+test('default - fallback with backslashes', async ({ t }) => {
+  var node = {
+    type: 'element',
+    tagName: 'slot',
+    attributes: [],
+    children: [{ type: 'text', content: '\\{text}.' }]
+  }
+
+  var opt = { store: new Map() }
+
+  slot(node, opt)
+
+  var content = node.content
+  var store = opt.store.get(content)
+
+  t.equal(content, '__::MASK_slot_0_::__')
+  t.equal(store, 'slots.default || `\\{text}.`')
+})
+
+test('default - fallback with expression', async ({ t }) => {
+  var node = {
+    type: 'element',
+    tagName: 'slot',
+    attributes: [],
+    children: [{ type: 'text', content: 'Some {{text}}.' }]
+  }
+
+  var opt = { store: new Map() }
+
+  slot(node, opt)
+
+  var content = node.content
+  var store = opt.store.get(content)
+
+  t.equal(content, '__::MASK_slot_0_::__')
+  t.equal(store, 'slots.default || `Some {{text}}.`')
+})
