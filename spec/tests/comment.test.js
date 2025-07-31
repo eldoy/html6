743205861b5b763b98ddd65fcbd4cbdaed935c42
Agustin <agustinbrage19@gmail.com>
2025-07-31 00:22:04 -0300

    test: add case on comments for mixed content

--- spec/tests/comment.test.js
+++ spec/tests/comment.test.js
@@ -110,3 +110,29 @@ test('content empty value', async ({ t }) => {
   t.equal(content, '__::MASK_comment_0_::__')
   t.equal(value, '<!---->')
 })
+
+test('content everything', async ({ t }) => {
+  var node = {
+    type: 'comment',
+    content: '`hello ${hello} \\{{hello}} {{hello}}'
+  }
+
+  var opt = { store: new Map() }
+
+  comment(node, opt)
+
+  var maskLiteral = '__::MASK_literal_0_::__'
+
+  var content = node.content
+  var value = opt.store.get(content)
+
+  t.equal(content, '__::MASK_comment_1_::__')
+  t.equal(
+    value,
+    '<!--' + '\\`hello \\${hello} {{hello}} ' + maskLiteral + '-->'
+  )
+
+  var value = opt.store.get(maskLiteral)
+
+  t.equal(value, 'hello')
+})
