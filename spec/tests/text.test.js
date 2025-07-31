b762e82ffe58c597c3c64c1eddf7222a87388354
Agustin <agustinbrage19@gmail.com>
2025-07-31 00:21:38 -0300

    feat: add text file for mask and expr on node text

---
+++ spec/tests/text.test.js
@@ -0,0 +1,135 @@
+var text = require('../../lib/text.js')
+
+test('simple content', async ({ t }) => {
+  var node = {
+    type: 'text',
+    content: 'hello'
+  }
+
+  var opt = { store: new Map() }
+
+  text(node, opt)
+
+  var content = node.content
+  var value = opt.store.get(node.content)
+
+  t.equal(content, '__::MASK_text_0_::__')
+  t.equal(value, 'hello')
+})
+
+test('content backticks', async ({ t }) => {
+  var node = {
+    type: 'text',
+    content: '`hello'
+  }
+
+  var opt = { store: new Map() }
+
+  text(node, opt)
+
+  var content = node.content
+  var value = opt.store.get(node.content)
+
+  var content = node.content
+  var value = opt.store.get(node.content)
+
+  t.equal(content, '__::MASK_text_0_::__')
+  t.equal(value, '\\`hello')
+})
+
+test('content dollar', async ({ t }) => {
+  var node = {
+    type: 'text',
+    content: '${hello}'
+  }
+
+  var opt = { store: new Map() }
+
+  text(node, opt)
+
+  var content = node.content
+  var value = opt.store.get(node.content)
+
+  t.equal(content, '__::MASK_text_0_::__')
+  t.equal(value, '\\${hello}')
+})
+
+test('content backslashes', async ({ t }) => {
+  var node = {
+    type: 'text',
+    content: '\\{{hello}}'
+  }
+
+  var opt = { store: new Map() }
+
+  text(node, opt)
+
+  var content = node.content
+  var value = opt.store.get(node.content)
+
+  t.equal(content, '__::MASK_text_0_::__')
+  t.equal(value, '{{hello}}')
+})
+
+test('content value', async ({ t }) => {
+  var node = {
+    type: 'text',
+    content: '{{item}}'
+  }
+
+  var opt = { store: new Map() }
+
+  text(node, opt)
+
+  var maskLiteral = '__::MASK_literal_0_::__'
+
+  var content = node.content
+  var value = opt.store.get(content)
+
+  t.equal(content, '__::MASK_text_1_::__')
+  t.equal(value, maskLiteral)
+
+  var value = opt.store.get(maskLiteral)
+
+  t.equal(value, 'item')
+})
+
+test('content empty value', async ({ t }) => {
+  var node = {
+    type: 'text',
+    content: '{{}}'
+  }
+
+  var opt = { store: new Map() }
+
+  text(node, opt)
+
+  var content = node.content
+  var value = opt.store.get(node.content)
+
+  t.equal(content, '__::MASK_text_0_::__')
+  t.equal(value, '')
+})
+
+test('content everything', async ({ t }) => {
+  var node = {
+    type: 'text',
+    content: '`hello ${hello} \\{{hello}} {{hello}}'
+  }
+
+  var opt = { store: new Map() }
+
+  text(node, opt)
+
+  var maskLiteral = '__::MASK_literal_0_::__'
+
+  var content = node.content
+  var value = opt.store.get(content)
+
+  t.equal(content, '__::MASK_text_1_::__')
+  t.equal(value, '\\`hello \\${hello} {{hello}} ' + maskLiteral)
+
+  var value = opt.store.get(maskLiteral)
+
+  t.equal(value, 'hello')
+})
