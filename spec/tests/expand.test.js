b7b211b8c7e9c17304a1aee2dc0c53d1def04679
Agustin <agustinbrage19@gmail.com>
2025-08-04 13:08:22 -0300

    feat: add if attr to components

--- spec/tests/expand.test.js
+++ spec/tests/expand.test.js
@@ -418,3 +418,42 @@ test('slot - empty value', async ({ t }) => {
   var expectedContent = '__::MASK_expand_0_::__'
   t.equal(content, expectedContent)
 })
+
+test('attributes - if', async ({ t }) => {
+  var node = {
+    type: 'element',
+    tagName: 'card',
+    attributes: [
+      { key: 'if', value: 'item' },
+      { key: 'project', value: 'empty' }
+    ],
+    children: []
+  }
+
+  var opt = {
+    store: new Map(),
+    components: { card: { fn: plain } }
+  }
+
+  expand(node, opt)
+
+  var content = node.content
+  var value = opt.store.get(content).replace(/\r/g, '')
+
+  var expectedVal = [
+    '${(function () {',
+    '  if (item) {',
+    '    return `${(function (props, slots) {',
+    '  with (props) {',
+    '    return `<div>title</div>`',
+    '  }',
+    '})({project: `empty`}, {}, _)}`',
+    '  }',
+    "  return ''",
+    '})()}'
+  ].join('\n')
+  t.equal(value, expectedVal)
+
+  var expectedContent = '__::MASK_expand_0_::__'
+  t.equal(content, expectedContent)
+})
